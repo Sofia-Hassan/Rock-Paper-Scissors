502f4b58a63d3026c2f057ff4d87da5d2d74058e
Sofia-Hassan <isofiahassan06@gmail.com>
2025-12-09 19:28:17 +0500

    Add user interface.
    Html elemets with little bit design are added.
    Limit game to 5 rounds and show winner is not added.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("Linked successfully")
-
 const value = ['rock','paper','scissors']
 
 function getComputerChoice(){
@@ -7,82 +5,84 @@ function getComputerChoice(){
   return value[random] 
 }
 
-// getComputerChoice()
-function getHumanChoice(){
-  let choice = prompt("What is your choice? rock paper or scissors");
-  return choice
-}
+const humanChoice = document.querySelectorAll(".select")
+humanChoice.forEach(btn =>{
+  btn.addEventListener("click", (e)=>
+    playRound(e.target.alt,getComputerChoice()))
+})
 
-// play game function ... I am not writting playRound inside play game 
-// because why do I need to ! now i got it...
+// function playGame(){
 
-function playGame(){
+  
 
-  let humanScore = 0, computerScore = 0
-
-  for( x = 1 ; x < 6 ; x ++){
+  // for(let x = 1 ; x < 6 ; x ++){
     
-    const humanSelection = (getHumanChoice()).toLowerCase()
-    const computerSelection = getComputerChoice()
-    console.log(`Round ${x}`)
-    playRound(humanSelection, computerSelection)
-  }
+  //   const humanSelection = (getHumanChoice()).toLowerCase()
+  //   const computerSelection = getComputerChoice()
+  //   console.log(`Round ${x}`)
+  //   playRound(humanSelection, computerSelection)
+  // }
   
-  let winner = humanScore > computerScore ? "you": "computer"
-  console.log(`winner is ${winner}`)
+//   let winner = humanScore > computerScore ? "you": "computer"
+//   console.log(`winner is ${winner}`)
 
-  console.log (`Rounds up!`)
+//   console.log (`Rounds up!`)
   
 
-  function playRound(humanChoice, computerChoice){
+// }
 
+// playGame()
+
+let humanScore = 0, computerScore = 0
+const result_div = document.querySelector(".result")
+const resultText = document.createElement("p")
+
+
+function playRound(humanChoice, computerChoice){
+
+  let result 
   if (humanChoice == computerChoice){
-    console.log (`Tie! nobody wins no body loses 
-      Your choice = ${humanChoice} and Computer choice = ${computerChoice}`)
+    result = `Tie! nobody wins no body loses 
+      Your choice = ${humanChoice} and Computer choice = ${computerChoice}`
   }
 
   // for rock as human choice
   else if(humanChoice === 'rock' && computerChoice === 'paper'){
     computerScore += 1
-    console.log (`You lose! Paper wraps Rock
-      Your choice = ${humanChoice} and Computer choice = ${computerChoice}`)
+    result = `You lose! Paper wraps Rock
+      Your choice = ${humanChoice} and Computer choice = ${computerChoice}`
     
   }
   else if(humanChoice === 'rock' && computerChoice === 'scissors'){
-  
-    console.log (`You wins! Rock shamashes Scissors
-      Your choice = ${humanChoice} and Computer choice = ${computerChoice}`)
+    result = `You wins! Rock shamashes Scissors
+      Your choice = ${humanChoice} and Computer choice = ${computerChoice}`
     humanScore += 1
-}
+  }
 
 // for paper as human choice
   else if(humanChoice === 'paper' && computerChoice === 'scissors'){
-  
-    console.log (`You lose! Scissors cuts Paper
-      Your choice = ${humanChoice} and Computer choice = ${computerChoice}`)
+    result = `You lose! Scissors cuts Paper
+      Your choice = ${humanChoice} and Computer choice = ${computerChoice}`
     computerScore += 1
   }
-    else if(humanChoice === 'paper' && computerChoice === 'rock'){
-  
-    console.log (`You Wins! Paper wraps Rock.
-      Your choice = ${humanChoice} and Computer choice = ${computerChoice}`)
+  else if(humanChoice === 'paper' && computerChoice === 'rock'){
+    result =  `You Wins! Paper wraps Rock.
+      Your choice = ${humanChoice} and Computer choice = ${computerChoice}`
     humanScore += 1
   }
 
-  // for scissors as human choice
-    else if(humanChoice === 'scissors' && computerChoice === 'paper'){
-  
-    console.log (`You Wins! Scissors cuts Paper
-      Your choice = ${humanChoice} and Computer choice = ${computerChoice}`)
+// for scissors as human choice
+  else if(humanChoice === 'scissors' && computerChoice === 'paper'){
+    result =  `You Wins! Scissors cuts Paper
+      Your choice = ${humanChoice} and Computer choice = ${computerChoice}`
     humanScore += 1
   }
-    else if(humanChoice === 'scissors' && computerChoice === 'rock'){
-  
-    console.log (`You lose! Rock shamashes Scissors 
-      Your choice = ${humanChoice} and Computer choice = ${computerChoice}`)
+  else if(humanChoice === 'scissors' && computerChoice === 'rock'){
+    result =  `You lose! Rock shamashes Scissors 
+      Your choice = ${humanChoice} and Computer choice = ${computerChoice}`
     computerScore += 1
   }
-}
-}
 
-playGame()
+  resultText.textContent = result
+  result_div.appendChild(resultText)
+}
