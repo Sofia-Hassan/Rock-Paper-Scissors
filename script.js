9438561afe68794d9b5e94981c058a71aa3c86c0
Sofia-Hassan <isofiahassan06@gmail.com>
2025-12-17 18:46:06 +0500

    Add remaing logic to play game for 5 rounds.
    Also add the play again option.

--- script.js
+++ script.js
@@ -8,81 +8,85 @@ function getComputerChoice(){
 const humanChoice = document.querySelectorAll(".select")
 humanChoice.forEach(btn =>{
   btn.addEventListener("click", (e)=>
-    playRound(e.target.alt,getComputerChoice()))
+    playGame(e.target.alt,getComputerChoice()))
 })
 
-// function playGame(){
+function playGame(x, y){
+   
+  playRound(x, y)
+  if (round == 5){
+    round = 0
+    humanChoice.forEach(btn => btn.disabled = true )
+    let winner = humanScore > computerScore ? "you": "computer"
+    finalWinner.textContent = `Rounds up! Winner is: ${winner}`
+    result_div.appendChild(finalWinner)
+    playAgainbtn.textContent = `Play again`
+    result_div.appendChild(playAgainbtn)
+    playAgainbtn.addEventListener('click',playAgain)
 
+  }
   
-
-  // for(let x = 1 ; x < 6 ; x ++){
-    
-  //   const humanSelection = (getHumanChoice()).toLowerCase()
-  //   const computerSelection = getComputerChoice()
-  //   console.log(`Round ${x}`)
-  //   playRound(humanSelection, computerSelection)
-  // }
   
-//   let winner = humanScore > computerScore ? "you": "computer"
-//   console.log(`winner is ${winner}`)
+}
 
-//   console.log (`Rounds up!`)
-  
-
-// }
-
-// playGame()
-
-let humanScore = 0, computerScore = 0
+let humanScore = 0, computerScore = 0, round = 0
 const result_div = document.querySelector(".result")
-const resultText = document.createElement("p")
+const resultText = document.createElement("h2")
+const resultChoices = document.createElement('p')
+const finalWinner = document.createElement('h1')
+const playAgainbtn = document.createElement('button')
+
 
 
 function playRound(humanChoice, computerChoice){
 
+  round += 1
   let result 
   if (humanChoice == computerChoice){
-    result = `Tie! nobody wins no body loses 
-      Your choice = ${humanChoice} and Computer choice = ${computerChoice}`
+    result = `Tie! nobody wins no body loses.`
   }
 
   // for rock as human choice
   else if(humanChoice === 'rock' && computerChoice === 'paper'){
     computerScore += 1
-    result = `You lose! Paper wraps Rock
-      Your choice = ${humanChoice} and Computer choice = ${computerChoice}`
+    result = `You lose! Paper wraps Rock.`
     
   }
   else if(humanChoice === 'rock' && computerChoice === 'scissors'){
-    result = `You wins! Rock shamashes Scissors
-      Your choice = ${humanChoice} and Computer choice = ${computerChoice}`
+    result = `You wins! Rock shamashes Scissors.`
     humanScore += 1
   }
 
 // for paper as human choice
   else if(humanChoice === 'paper' && computerChoice === 'scissors'){
-    result = `You lose! Scissors cuts Paper
-      Your choice = ${humanChoice} and Computer choice = ${computerChoice}`
+    result = `You lose! Scissors cuts Paper.`
     computerScore += 1
   }
   else if(humanChoice === 'paper' && computerChoice === 'rock'){
-    result =  `You Wins! Paper wraps Rock.
-      Your choice = ${humanChoice} and Computer choice = ${computerChoice}`
+    result =  `You Wins! Paper wraps Rock.`
     humanScore += 1
   }
 
 // for scissors as human choice
   else if(humanChoice === 'scissors' && computerChoice === 'paper'){
-    result =  `You Wins! Scissors cuts Paper
-      Your choice = ${humanChoice} and Computer choice = ${computerChoice}`
+    result =  `You Wins! Scissors cuts Paper.`
     humanScore += 1
   }
   else if(humanChoice === 'scissors' && computerChoice === 'rock'){
-    result =  `You lose! Rock shamashes Scissors 
-      Your choice = ${humanChoice} and Computer choice = ${computerChoice}`
+    result =  `You lose! Rock shamashes Scissors.`
     computerScore += 1
   }
 
-  resultText.textContent = result
+  resultText.textContent = `Round ${round} ` + result
+  resultChoices.textContent = `Your Choice: ${humanChoice} \u00A0\u00A0\u00A0\u00A0\u00A0 Computer Choice: ${computerChoice}`
   result_div.appendChild(resultText)
+  result_div.appendChild(resultChoices)
+}
+
+function playAgain(){
+  humanChoice.forEach(btn => btn.disabled = false )
+  result_div.removeChild(resultText)
+  result_div.removeChild(resultChoices)
+  result_div.removeChild(finalWinner)
+  result_div.removeChild(playAgainbtn)
 }
